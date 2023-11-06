c5dc713c95c90b83f900d40b1bb9051228aeadf8
Nabeel Nasir <69030093+nabeelnasir9@users.noreply.github.com>
2023-11-06 18:46:39 +0500

    second commit

---
+++ JobsPortal/components/home/welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './welcome.style'
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Nabeel Nasir</Text>
+    </View>
+  )
+}
+
+export default Welcome
